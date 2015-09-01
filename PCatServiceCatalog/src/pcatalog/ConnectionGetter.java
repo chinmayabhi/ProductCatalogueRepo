c29d9bf6f2fb84ed7cd62a0d0f757f251f213ad6
test <test@gmail.com>
2015-08-31 22:51:57 -0700

    xe

--- PCatServiceCatalog/src/pcatalog/ConnectionGetter.java
+++ PCatServiceCatalog/src/pcatalog/ConnectionGetter.java
@@ -14,7 +14,7 @@ public class ConnectionGetter
 	try{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		con = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:orcl", "PCatalogue", "password" );
+					"jdbc:oracle:thin:@localhost:1521:xe", "PCatalogue", "password" );
 					
 		
 		
